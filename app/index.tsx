7399d081397f18951972ac12d5b28628ed488682
Leonardo Sarto <leonardo@apicesistemas.com.br>
2024-10-14 17:59:06 -0300

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,49 @@
+import * as React from "react";
+import { Linking, View } from "react-native";
+import { Button } from "~/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card";
+import { Text } from "~/components/ui/text";
+import { Input } from "~/components/ui/input";
+import { Label } from "~/components/ui/label";
+import { Separator } from "~/components/ui/separator";
+import { H4, P, Small } from '~/components/ui/typography';
+export default function Screen() {
+  return (
+    <View className="flex-1 justify-center items-center bg-secondary/30">
+      <Card className="w-full max-w-sm p-6 rounded-2xl">
+        <CardContent className="flex-col gap-4 items-end">
+          <Input
+            className="w-full"
+            placeholder="Username"
+            aria-labelledby="username"
+            aria-errormessage="inputError"
+          />
+          <Input
+            className="w-full"
+            placeholder="Password"
+            aria-labelledby="username"
+            aria-errormessage="inputError"
+          />
+          <Label>Forget password?</Label>
+        </CardContent>
+        <CardFooter className="flex-col gap-3 pb-0">
+          <Button>
+            <Text>Login</Text>
+          </Button>
+          <Separator></Separator>
+          <Text>Doesn't have an account?</Text>
+          <Button>
+            <Text>Register</Text>
+          </Button>
+        </CardFooter>
+      </Card>
+    </View>
+  );
+}
---
+++ app/screens/login.tsx
@@ -0,0 +1,51 @@
+import { View } from "lucide-react-native";
+import React from "react";
+import Animated, { LayoutAnimationConfig } from "react-native-reanimated";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card";
+import { Text } from "~/components/ui/text";
+
+export function Login() {
+  const [value, setValue] = React.useState("");
+
+  const onChangeText = (text: string) => {
+    setValue(text);
+  };
+
+  return (
+    <View className="flex-1 justify-center items-center gap-5 p-6 bg-secondary/30">
+      <Card className="w-full max-w-sm p-6 rounded-2xl">
+        <CardHeader className="items-center">
+          <View className="p-3" />
+          <CardTitle className="pb-2 text-center">Rick Sanchez</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <View className="flex-row justify-around gap-3">
+            <View className="items-center">
+              <Text className="text-sm text-muted-foreground">Dimension</Text>
+              <Text className="text-xl font-semibold">C-137</Text>
+            </View>
+            <View className="items-center">
+              <Text className="text-sm text-muted-foreground">Age</Text>
+              <Text className="text-xl font-semibold">70</Text>
+            </View>
+            <View className="items-center">
+              <Text className="text-sm text-muted-foreground">Species</Text>
+              <Text className="text-xl font-semibold">Human</Text>
+            </View>
+          </View>
+        </CardContent>
+        <CardFooter className="flex-col gap-3 pb-0">
+          <View className="flex-row items-center overflow-hidden"></View>
+          <View />
+        </CardFooter>
+      </Card>
+    </View>
+  );
+}
