7b287cdb654c4a0ecf7c052ddcb093409f915d56
Leonardo Sarto <leonardosarto1@gmail.com>
2025-02-26 09:43:22 -0300

    Changed app logo

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
         debugShowCheckedModeBanner: false,
         theme: const ThemeComponent(TextTheme(
                 labelSmall: TextStyle(
